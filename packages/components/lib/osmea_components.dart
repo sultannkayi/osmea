ba2189f4c8b05a2c438ab229258d27068d141bd1
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-06-13 01:29:38 +0300

    remove: deprecated components

---
+++ packages/components/lib/osmea_components.dart
@@ -0,0 +1,8 @@
+library osmea_components;
+
+// Theme
+export 'src/theme/colors.dart';
+export 'src/theme/theme.dart';
+export 'src/theme/theme_provider.dart';
+export 'src/theme/typography.dart';
+export 'src/theme/component_dimensions.dart'; 
