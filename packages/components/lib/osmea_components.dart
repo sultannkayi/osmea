2260c1d1991aabb6e1cd9d230dc7406850dc2507
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-06-17 21:12:29 +0300

    feat: enhance component exports and add unified text handling method

--- packages/components/lib/osmea_components.dart
+++ packages/components/lib/osmea_components.dart
@@ -14,7 +14,10 @@ export 'src/styles/colors.dart';
 export 'src/theme/theme.dart';
 
 // 🔘 Buttons
-// (Button components can be exported here)
+export 'src/components/buttons/button.dart';
+
+// 📝 Text
+export 'src/components/text/text.dart';
 
 // 🧭 Navigation
 export 'src/components/navbar/navbar.dart';
@@ -29,19 +32,29 @@ export 'src/components/radio_button/radio_button.dart';
 // Checkbox component
 export 'src/components/checkbox/checkbox.dart';
 
+// 🔑 Login Button
+export 'src/components/login_button/login_button.dart';
+
 // 🛠️ Utils
 // Utility functions and extensions
 export 'src/utils/sizer_extensions.dart';
+export 'src/utils/text_extensions.dart';
+export 'src/utils/button_size_extensions.dart';
+export 'src/utils/icon_positions_extensions.dart';
 export 'src/utils/switch_size_extensions.dart';
 export 'src/utils/navbar_extensions.dart';
 export 'src/utils/radio_size_extensions.dart';
 export 'src/utils/checkbox_size_extensions.dart';
+export 'src/utils/system_extension.dart';
 
 // Central export for all components
 export 'src/components.dart';
 
 // Enums and Cubit files
 export 'src/enums/enums.dart';
+
+// 🧩 Cubit
 export 'src/components/login_button/cubit/login_button_cubit.dart';
 export 'src/core/cubit_button/cubit/core_button_state.dart';
 export 'src/core/cubit_button/cubit/core_button_cubit.dart';
+
