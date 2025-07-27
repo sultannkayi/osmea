00738717f4ace9dad7171a4b9fea8c004f859a8c
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2025-07-27 19:53:49 +0300

    feat: enhance SplashView routing with environment details and initialization

--- projects/admin_dashboard/lib/routes/app_routes.dart
+++ projects/admin_dashboard/lib/routes/app_routes.dart
@@ -1,11 +1,14 @@
 // Define the GoRouter
 import 'package:admin_dashboard/views/view_splash/splash_view.dart';
 import 'package:core/core.dart';
+import 'package:flavor/flavor.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 // network page  configs serive
 final GoRouter appRouter = GoRouter(
+  initialLocation: '/',
+  // Global route configuration
   routes: <RouteBase>[
     // GoRoute(
     //   path: '/home',
@@ -23,8 +26,15 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/',
       builder: (BuildContext context, GoRouterState state) {
+        final flavor = Flavor.I;
+        final isDev = flavor.environment == Environment.dev;
+
         return SplashView(
-          arguments: {"title": "Splash View"},
+          arguments: {
+            "title": "Splash View",
+            "isDev": isDev,
+            "environment": flavor.environment.toString(),
+          },
           currentView: MasterViewTypes.content,
         );
       },
